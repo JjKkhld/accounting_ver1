37a4014baee24311bd493d4b55ba10499798533f
JjKkhld <kholod.zhenya18@gmail.com>
2024-10-26 12:09:42 +0200

    The first version

---
+++ Main_ver-1.py
@@ -0,0 +1,115 @@
+# libraries
+import telebot
+import sqlite3
+
+# connecting with a bot
+bot = telebot.TeleBot('8158733015:AAHhE_5JMUCs0rRXL8zGocmZo27JpxI0POc')
+
+
+# creating menu of the categories (command - start)
+@bot.message_handler(commands=['start'])
+def start(message):
+    conn = sqlite3.connect('base.sql')
+    cur = conn.cursor()
+
+    cur.execute('CREATE TABLE IF NOT EXISTS records (id INTEGER PRIMARY KEY, '
+                'date DATE, category varchar(50), amount float)')
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    # category buttons
+    markup = telebot.types.InlineKeyboardMarkup()
+    cat_btn1 = telebot.types.InlineKeyboardButton('Housing & Studies', callback_data='Housing & Studies')
+    cat_btn2 = telebot.types.InlineKeyboardButton('Groceries', callback_data='Groceries')
+    cat_btn3 = telebot.types.InlineKeyboardButton('Transportation', callback_data='Transportation')
+    markup.row(cat_btn1, cat_btn2, cat_btn3)
+    cat_btn4 = telebot.types.InlineKeyboardButton('Clothing & Shoes', callback_data='Clothing & Shoes')
+    cat_btn5 = telebot.types.InlineKeyboardButton('Body Care & Med.', callback_data='Body Care & Med.')
+    cat_btn6 = telebot.types.InlineKeyboardButton('Media & Washing', callback_data='Media & Washing')
+    markup.row(cat_btn4, cat_btn5, cat_btn6)
+    cat_btn7 = telebot.types.InlineKeyboardButton('Fun & Vacation', callback_data='Fun & Vacation')
+    cat_btn8 = telebot.types.InlineKeyboardButton('Emergency', callback_data='Emergency')
+    cat_btn9 = telebot.types.InlineKeyboardButton('Other', callback_data='Other')
+    markup.row(cat_btn7, cat_btn8, cat_btn9)
+    cat_btn10 = telebot.types.InlineKeyboardButton('Check the data base', callback_data='check')
+    markup.add(cat_btn10)
+
+    bot.send_message(message.chat.id, "Choose the category below:", reply_markup=markup)
+
+
+# making a functionality of the buttons and providing a data (category, amount)
+@bot.callback_query_handler(func=lambda callback: callback.data in ['Housing & Studies', 'Groceries', 'Transportation',
+                                                                    'Clothing & Shoes', 'Body Care & Med.',
+                                                                    'Media & Washing', 'Fun & Vacation',
+                                                                    'Emergency', 'Other', 'check'])
+def callback_message(callback):
+    if callback.data != 'check':
+        # inform the user to enter the amount
+        bot.send_message(callback.message.chat.id, f"Great, record {callback.data} was saved! Enter the amount:")
+
+        # store `callback.data` in a global or state to access it later in `amount`
+        bot.register_next_step_handler(callback.message, amount, callback.data)
+    else:
+        # query database and show records
+        conn = sqlite3.connect('base.sql')
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM records")
+        records = cur.fetchall()
+        cur.close()
+        conn.close()
+
+        if records:
+            info = ''
+            for el in records:
+                info += f"<b>ID</b>: {el[0]}; <b>DATE</b>: {el[1]}; <b>CATEGORY</b>: {el[2]}; <b>AMOUNT</b>: {el[3]}\n"
+            bot.send_message(callback.message.chat.id, info, parse_mode='html')
+            start(callback.message)
+        else:
+            bot.send_message(callback.message.chat.id, "Database is empty!")
+            start(callback.message)
+
+
+def amount(message, category):
+    # process and save the amount to the database
+    num = message.text
+    bot.send_message(message.chat.id, f"Ok, the amount {num} was added to data base!")
+
+    # database operations with parameterized query
+    conn = sqlite3.connect('base.sql')
+    cur = conn.cursor()
+    cur.execute("INSERT INTO records (category, amount) VALUES('%s', '%s')" % (category, num))
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    cont(message)
+
+
+def cont(message):
+    # 'yes'&'no' buttons
+    markup = telebot.types.InlineKeyboardMarkup()
+    end_btn1 = telebot.types.InlineKeyboardButton('Yes', callback_data='yes')
+    end_btn2 = telebot.types.InlineKeyboardButton('No', callback_data='no')
+    markup.row(end_btn1, end_btn2)
+    markup.add(telebot.types.InlineKeyboardButton('Ckeck the database', callback_data='check'))
+    bot.send_message(message.chat.id, "Do you want to continue?", reply_markup=markup)
+
+
+@bot.callback_query_handler(func=lambda callback: callback.data in ['yes', 'no', 'check'])
+def callbck_con(callback):
+    if callback.data == 'yes':
+        start(callback.message)
+    elif callback.data == 'no':
+        pass
+    else:
+        # query database and show records
+        conn = sqlite3.connect('base.sql')
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM records")
+        records = cur.fetchall()
+        cur.close()
+        conn.close()
+
+
+bot.infinity_polling()
